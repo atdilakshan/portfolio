639b7e4c2af6375d7df69cf04bc96afcbebff71d
atdilakshan <dilakshan0501@gmail.com>
2022-09-12 15:23:22 +0530

    final

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import ReactTyped from "react-typed";
+import { useTranslation } from "react-i18next";
 
 // components
 import Avatar from "./Avatar";
@@ -10,15 +11,15 @@ export interface Props {
 }
 
 const Hero = ({ className }: Props) => {
+  const { t } = useTranslation();
+
   return (
     <div className={clsx(className)}>
       <Avatar />
       <Title />
-      <ReactTyped className={clsx(className, 
-      "flex justify-center",
-      "text-2xl"
-      )}
-        strings={["Developer", "Youtuber", ""]}
+      <ReactTyped
+        className={clsx(className, "flex justify-center", "text-2xl")}
+        strings={[t("Hero.subtitle.dev"), t("Hero.subtitle.youtuber")]}
         typeSpeed={100}
         backSpeed={100}
         loop
